0131171f90d3d88510d9db6a2391063bf71da8df
Bill MacAllister <bill@ca-zephyr.org>
2011-10-09 00:58:32 -0700

    Make it simple to select new pictures.

--- cgi-bin/page_top.php
+++ cgi-bin/page_top.php
@@ -23,6 +23,7 @@
    &nbsp;<a href="picture_reload">Re-Load</a><br>
    &nbsp;<a href="picture_maint">Maint</a><br>
    &nbsp;<a href="picture_sort">Sort</a><br>
+   &nbsp;<a href="picture_sort?in_new=1">New Pictures</a><br>
    <br>
    &nbsp;<a href="picture_path">Path</a><br>
    &nbsp;<a href="picture_search">Find</a><br>
