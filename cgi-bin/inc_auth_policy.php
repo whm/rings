c495ded2febed9e1293904c63707ffcb4d270b7d
Bill MacAllister <whm@stanford.edu>
2014-12-30 13:40:30 -0800

    Remove dependency on custom php sessons

--- cgi-bin/inc_auth_policy.php
+++ cgi-bin/inc_auth_policy.php
@@ -2,7 +2,6 @@
 //
 // Open a session check authorization
 $whm_base = '/usr/share/whm-ldapauth-php/cgi-bin';
-require("$whm_base/whm_php_sessions.inc");
 require("$whm_base/whm_php_auth.inc");
 require('inc_util.php');
 
