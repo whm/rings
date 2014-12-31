359a148fe5a08af174dc734e9772939311b7684e
Bill MacAllister <whm@stanford.edu>
2014-12-30 20:52:28 -0800

    Correct connect error handling

--- cgi-bin/inc_db_connect.php
+++ cgi-bin/inc_db_connect.php
@@ -1,7 +1,8 @@
 <?PHP
 // connect to the database
 $DBH = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
-if ($mysqli->connect_error) {
-    $_SESSION[msg] .= "<br>Error connecting to MySQL host $mysql_host";
+if ($DBH->connect_errno) {
+    $_SESSION['msg'] .= "\n<br>ERROR: connecting to MySQL host $mysql_host";
+    $_SESSION['msg'] .= "\n<br>MySQL ERROR: " . $DBH->connect_error;
 }
 ?>
