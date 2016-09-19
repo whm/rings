3641fa0e8f4610676c8017767c56188fccab64c6
Bill MacAllister <whm@stanford.edu>
2016-09-19 00:09:32 -0700

    Add support for files based rings
    Update the ring export from db to files.
    Update the ring load to support creating files.

--- module/Rings/Common.pm
+++ module/Rings/Common.pm
@@ -29,12 +29,18 @@ BEGIN {
                      db_connect
                      db_disconnect
                      dbg
+                     check_picture_size
+                     create_picture_dirs
                      get_config
+                     get_meta_data
                      get_next_id
                      get_picture_sizes
+                     get_picture_types
                      msg
+                     pid_to_path
                      sql_datetime
                      store_meta_data
+                     store_picture
                      trim
                      unix_seconds
                      );
@@ -285,39 +291,54 @@ sub sql_datetime {
 }
 
 # ----------------------------------------------------------------------
-# store pictures
+# Selecting the next picture in a ring requires that the date_taken
+# and the picture_sequence pair be unique. This routine searches the
+# existing picture database and returns the number of entries for a
+# given date +1.
 
-sub store_meta_data {
+sub get_picture_sequence {
+    my ($dt) = @_;
+    my $seq = 1;
+    my $sel = "SELECT pid FROM pictures_information WHERE date_taken = ? ";
+    my $sth = $DBH->prepare ($sel);
+    if ($CONF->debug) {dbg($sel);}
+    $sth->execute($dt);
+    while (my $row = $sth->fetchrow_hashref) {
+        $se1++;
+    }
+    return $seq;
+}
 
-    my ($in_file, $pid) = @_;
+# ----------------------------------------------------------------------
+# Get meta data from picture and return a hash with the data.
 
-    dbg (" Processing $in_file");
+sub get_meta_data {
+
+    my ($in_blob) = @_;
+
+    # Data returned will be passed in a hash
+    my %ret = ();
+    
+    dbg (" Getting meta data for $in_file");
     if (! -e $in_file) {
         msg("ERROR: $in_file not found");
         return;
     }
 
-    # Set file paths and names
-    my $source_path = File::Spec->rel2abs( $in_file ) ;
-    my ($source_file, $source_dirs, $source_suffix) = fileparse($source_file);
-    my @dirs = split(/\//, $source_dirs);
-    my $picture_lot = @dirs[-1];
-
-    # Read in the file
-    my $this_picture = read_file("$this_path/$this_file", binmode => ':raw');
+    $ret{'picture'} = $in_blob;
 
     # Get picture meta data
     my @blob;
-    $blob[0] = $this_picture;
+    $blob[0] = $ret{'picture'};
     my $pic = Image::Magick->New();
     $pic->BlobToImage(@blob);
 
-    my ($width,
-        $height,
-        $size,
-        $format,
-        $compression,
-        $signature)
+    my ($ret{'width'},
+        $ret{'height'},
+        $ret{'size'},
+        $ret{'format'},
+        $ret{'compression'},
+        $ret{'signature'})
         = $thisPic->Get('width',
                         'height',
                         'filesize',
@@ -325,111 +346,230 @@ sub store_meta_data {
                         'compression',
                         'signature');
     if ($CONF->debug) {
-        dbg ("      format: $format");
-        dbg (" compression: $compression");
-        dbg ("       width: $width");
-        dbg ("      height: $height");
+        dbg ('      format: ' . $ret{'format'});
+        dbg (' compression: ' . $ret{'compression'});
+        dbg ('       width: ' . $ret{'width'});
+        dbg ('      height: ' . $ret{'height'});
     }
 
     my $info = ImageInfo(\@blob[0]);
-    my $camera            = ${$info}{'Model'};
-    my $this_datetime     = ${$info}{'CreateDate'};
-    my $this_shutterspeed = ${$info}{'ShutterSpeed'};
-    my $this_fnumber      = ${$info}{'FNumber'};
-    my $this_raw_size     = length $blob[0];
+    my $ret{'camera'}       = ${$info}{'Model'};
+    my $ret{'datetime'}     = ${$info}{'CreateDate'};
+    my $ret{'shutterspeed'} = ${$info}{'ShutterSpeed'};
+    my $ret{'fnumber'}      = ${$info}{'FNumber'};
+    my $ret{'size'}         = length $blob[0];
     if ($CONF->debug) {
-        dbg ("        size: $this_raw_size");
-        dbg ("       model: $camera");
-        dbg ("    datetime: $this_datetime");
-        dbg ("exposuretime: $this_shutterspeed");
-        dbg ("     fnumber: $this_fnumber");
-        dbg ("EXIF Information ==============================");
+        dbg ('        size: ' . $ret{'size'});
+        dbg ('       model: ' . $ret{'camera'});
+        dbg ('    datetime: ' . $ret{'datetime'});
+        dbg ('exposuretime: ' . $ret{'shutterspeed'});
+        dbg ('     fnumber: ' . $ret{'fnumber'});
+        dbg ('EXIF Information ==============================');
         foreach my $t (keys %{$info}) {
             print "$t = ${$info}{$t}\n";
         }
-        dbg ("EXIF Information end ==========================");
-    }
-    if (!$this_datetime) {
-        $this_datetime = sql_datetime();
+        dbg ('EXIF Information end ==========================');
     }
 
-    # Store meta data
-    if ($pid) {
-        my $sel = "SELECT pid FROM pictures_prints WHERE pid=? ";
-        my $sth = $DBH->prepare ($sel);
-        if ($CONF->debug) {dbg($sel);}
-        $sth->execute($thisPID);
-        my $row_found;
-        while (my $row = $sth->fetchrow_hashref) {$row_found = 1;}
-        if (!$row_found) {
-            msg("ERROR: $pid not found when attempting meta data update");
-            return;
-        }
+    return %ret;
+}
+
+# ----------------------------------------------------------------------
+# Store meta data for a picture
+
+sub store_meta_data {
+
+    my ($pid, $meta_data_ref) = @_;
+    my %meta    = %{ $meta_data_ref };
+    my $in_file = $meta{'in_file'};
+    
+    dbg (" Storing meta data for $in_file");
+
+    # Set file paths and names
+    my $ret{'source_path'} = File::Spec->rel2abs( $in_file ) ;
+    my ($ret{'source_file'},
+        $ret{'source_dirs'},
+        $ret{'source_suffix'}) = fileparse($ret{'source_path'});
+    my @dirs = split(/\//, $ret{'source_dirs'});
+    my $ret{'picture_lot'} = @dirs[-1];
+
+    # Set default time stamp
+    if (!$ret{'datetime'}) {
+        $ret{'datetime'} = sql_datetime();
+    }
+    
+    # Store meta data if the PID is passed
+    my $sel = "SELECT pid FROM pictures_information WHERE pid=? ";
+    my $sth = $DBH->prepare ($sel);
+    if ($CONF->debug) {dbg($sel);}
+    $sth->execute($thisPID);
+    my $row_found;
+    while (my $row = $sth->fetchrow_hashref) {$row_found = 1;}
+    if ($row_found) {
         my $cmd = "UPDATE pictures_information SET ";
-        $cmd .= 'source_file = ?,';
-        $cmd .= 'picture_lot = ?,';
-        $cmd .= 'file_name = ?,';
+        $cmd .= 'picture_date = ?,';
         $cmd .= 'camera = ?,';
         $cmd .= 'shutter_speed = ?,';
         $cmd .= 'fstop = ?,';
         $cmd .= 'date_last_maint = ?,';
-        $cmd .= 'WHERE pid = ? ';
+        $cmd .= 'WHERE pid = ?,';
         my $sth_update = $DBH_UPDATE->prepare ($cmd);
         if ($CONF->debug) {dbg($cmd);}
-        if ($CONF->update) {
-            $sth_update->execute($source_file,
-                                 $picture_lot,
-                                 $file_name,
-                                 $camera,
-                                 $this_shutter_speed,
-                                 $this_fstop,
+        $sth_update->execute(
+            $ret{'datetime'},
+            $ret{'camera'},
+            $ret{'shutter_speed'},
+            $ret{'fstop'},
+            sql_datetime(),
+            $pid
+        );
+    } else {
+        # Get a picture sequence number
+        my $picture_sequence = get_picture_sequence($ret{'datetime'});
+        my $cmd = "INSERT INTO pictures_information SET ";
+        $cmd .= 'pid = ?,';
+        $cmd .= 'picture_lot = ?,';
+        $cmd .= 'date_taken = ?,';
+        $cmd .= 'picture_date = ?,';
+        $cmd .= 'picture_sequence = ?,';
+        $cmd .= 'source_file = ?,';
+        $cmd .= 'file_name = ?,';
+        $cmd .= 'camera = ?,';
+        $cmd .= 'shutter_speed = ?,';
+        $cmd .= 'fstop = ?,';
+        $cmd .= 'grade = ?,';
+        $cmd .= 'public = ?,';
+        $cmd .= 'date_last_maint = ?,';
+        $cmd .= 'date_added = ? ';
+        my $sth_update = $DBH_UPDATE->prepare ($cmd);
+        if ($CONF->debug) {dbg($cmd);}
+        $sth_update->execute(
+            $pid,
+            $ret{'picture_lot'},
+            $ret{'datetime'},
+            $ret{'datetime'},
+            $picture_sequence,
+            $ret{'source_path'},
+            $ret['source_file'},
+            $ret{'camera'},
+            $ret{'shutter_speed'},
+            $ret{'fstop'},
+            $CONF->default_grade,
+            $CONF->default_public,
+            sql_datetime(),
+            sql_datetime()
+        );
+    }
+
+    return %ret;
+}
+
+# ------------------------------------------------
+# store pictures
+
+sub store_picture {
+
+    my ($this_pid, $this_size_id, $this_picture, $this_type) = @_;
+    
+    dbg (" Processing $this_pid $this_size_id");
+
+    my $max_x;
+    my $max_y;
+    my $table;
+    my $sel = 'SELECT * FROM picture_sizes WHERE size_id = ?';
+    my $sth = $DBH->prepare ($sel);
+    if ($CONF->debug) {dbg($sel);}
+    $sth->execute($this_size_id);
+    if (my $row = $sth->fetchrow_hashref) {
+        $max_y = $row->{max_height};
+        $max_x = $row->{max_width};
+        $table = $row->{table};
+    }
+    if (!$max_y || !$max_x || !$table) {
+        msg('fatal', "Invalid size id: $this_size_id");
+    }
+    
+    my @blob;
+    $blob[0] = $this_picture;
+    my $this_pic = Image::Magick->New();
+    $this_pic->BlobToImage(@blob);
+
+    # Set the size parameters
+    my $newPic   = $thisPic->Clone();
+    my $x = $width;
+    my $y = $height;
+    my $x1 = $max_x;
+    my $y1 = int(($x1/$width) * $height);
+    my $y2 = $max_y;
+    my $x2 = int(($y2/$height) * $width);
+    if ($x1 < $x2) {
+        $x = $x1;
+        $y = $y1;
+    } else {
+        $x = $x2;
+        $y = $y2;
+    }
+    dbg (" Producing picture $x by $y ");
+    $newPic->Resize(width=>$x, height=>$y);
+    my @bPic  = $newPic->ImageToBlob();
+    
+    my $sel = "SELECT pid FROM $thisTable ";
+    $sel .= "WHERE pid=$thisPID ";
+    my $sth = $dbh->prepare ($sel);
+    if ($opt_debug) {dbg($sel);}
+    $sth->execute();
+    
+    my $row = $sth->fetchrow_hashref;
+
+    if ($row->{pid} != $this_pid && $this_pid > 0) {
+        
+        my $cmd = "INSERT INTO $this_table (";
+        $cmd .= 'pid,';
+        $cmd .= 'picture_type,';
+        $cmd .= 'width,';
+        $cmd .= 'height,';
+        $cmd .= 'size,';
+        $cmd .= 'date_last_maint,';
+        $cmd .= 'date_added';
+        $cmd .= ') VALUES (?,?,?,?,?,?,?) ';
+        my $sth_update = $dbh_update->prepare ($cmd);
+        if ($opt_debug) {dbg($cmd);}
+        if ($opt_update) {
+            $sth_update->execute(
+                                 $this_pid,
+                                 $this_type,
+                                 $x,
+                                 $y,
+                                 length($bPic[0]),
                                  sql_datetime(),
-                                 $pid
-                );
+                                 sql_datetime()
+                                 );
         }
-        return $pid;
+        
+    } elsif ($thisPID > 0) {
+        
+        my $cmd = "UPDATE $thisTable SET ";
+        $cmd .= 'picture_type = ?,';
+        $cmd .= 'width = ?,';
+        $cmd .= 'height = ?,';
+        $cmd .= 'size = ?,';
+        $cmd .= 'date_last_maint = ? ';
+        $cmd .= 'WHERE pid = ? ';
+        my $sth_update = $dbh_update->prepare ($cmd);
+        if ($opt_debug) {dbg($cmd);}
+        if ($opt_update) {
+            $sth_update->execute(
+                                 $this_type,
+                                 $x,
+                                 $y,
+                                 length($bPic[0]),
+                                 sql_datetime(),
+                                 $this_pid,
+                                 );
+        }
     }
-
-    # Set the picture sequence
-    # TODO
-
-    $pid = get_next_id("pid");
-    my $cmd = 'INSERT INTO pictures_information SET ';
-    $cmd .= 'pid = ?,';
-    $cmd .= 'source_file = ?,';
-    $cmd .= 'picture_lot = ?,';
-    $cmd .= 'file_name = ?,';
-    $cmd .= 'date_taken = ?,';
-    $cmd .= 'picture_date = ?,';
-    $cmd .= 'picture_sequence = ?,';
-    $cmd .= 'grade = ?,';
-    $cmd .= 'public = ?,';
-    $cmd .= 'camera = ?,';
-    $cmd .= 'shutter_speed = ?,';
-    $cmd .= 'fstop = ?,';
-    $cmd .= 'date_last_maint = ?,';
-    $cmd .= 'date_added = ? ';
-    my $sth_update = $DBH_UPDATE->prepare ($cmd);
-    if ($CONF->debug) {dbg($cmd);}
-    if ($CONF->update) {
-        $sth_update->execute($pid,
-                             $source_path,
-                             $picture_lot,
-                             $source_file,
-                             $this_date,
-                             $this_date,
-                             $picture_sequence,
-                             $CONF->default_grade,
-                             $CONF->default_public,
-                             $this_camera,
-                             $this_shutter_speed,
-                             $this_fstop,
-                             sql_datetime(),
-                             sql_datetime()
-            );
-    }
-
-    return $pid;
+    File::Copy($bPic[0], $path);
+    return;
 }
 
 # ----------------------------------------------------------------------
@@ -464,18 +604,108 @@ sub unix_seconds {
 }
 
 # ----------------------------------------------------------------------
+# Create directories need to store a picture
+
+sub _create_dir {
+    my ($this_dir) = @_;
+    $this_dir =~ s{//}{/}xmsg;
+    if (!-e $this_root) {
+        mkdir $this_root;
+        if (!-d $this_root) {
+            msg ('fatal', "Problem creating directory $this_root")
+        }
+    }
+    return $this_dir;
+}
+
+# ----------------------------------------------------------------------
+# Create directories need to store a picture
+
+sub create_picture_dirs {
+    my ($group, $this_size) = @_;
+    if (!check_picture_size($this_size)) {
+        msg('fatal', "Invalid size: $this_size");
+    }
+
+    my $output_root = $CONF->picture_root;
+    $output_root = _create_dir($output_root);
+
+    $output_root .= "/$group";
+    $output_root = _create_dir($output_root);
+
+    $output_root = "$output_root/$this_size";
+    $output_root = _create_dir($output_root);
+
+    return $output_root;
+}
+
+# ----------------------------------------------------------------------
+# valid the picure size
+
+sub check_picture_size {
+    my ($this_size) = @_;
+    
+    my $sel = 'SELECT * FROM picture_sizes where max_size = ?';
+    my $sth = $DBH->prepare ($sel);
+    if ($CONF->debug) {dbg($sel);}
+    $sth->execute($this_size);
+    my $size_found;
+    while (my $row = $sth->fetchrow_hashref) {
+        $size_found++;
+    }
+    return $size_found;
+}
+
+# ----------------------------------------------------------------------
 # get picture sizes to generate
 
 sub get_picture_sizes {
+    my @flds = ('max_height', 'max_width', 'table', 'description');
     my $sel = 'SELECT * FROM picture_sizes';
     my $sth = $DBH->prepare ($sel);
     my %psizes = ();
     if ($CONF->debug) {dbg($sel);}
     $sth->execute();
     while (my $row = $sth->fetchrow_hashref) {
-        $psizes{$row->{max_size}} = $row->{description};
+        for my $f (@flds) {
+            $psizes{$row->{size_id}}{$f} = ${$row}{$f};
+        }
     }
     return %psizes;
+}
+
+# ----------------------------------------------------------------------
+# get picture types
+
+sub get_picture_types {
+    my $sel = 'SELECT * FROM picture_types';
+    my $sth = $DBH->prepare ($sel);
+    my %mime_types = ();
+    if ($CONF->debug) {dbg($sel);}
+    $sth->execute();
+    while (my $row = $sth->fetchrow_hashref) {
+        $mime_types{$row->{mime_type}} = $row->{file_type};
+    }
+    return %mime_types;
+}
+
+# ----------------------------------------------------------------------
+# Return the path to a file given the pid, group, size, and type desired
+
+sub pid_to_path {
+    my ($pid, $group, $max_size, $type) = @_;
+
+    if (!check_picture_size($max_size)) {
+        msg('fatal', "Invalid size: $this_size");
+    }
+    
+    $type = s/[.]//xmsg;
+    
+    my $path = $CONF->picture_root;
+    $path .= "/${group}/${size}/${pid}.${type}";
+    $path =~ s{//}{/}xmsg;
+    
+    return $path;
 }
 
 # ----------------------------------------------------------------------
