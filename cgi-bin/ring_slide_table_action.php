f70691393e6b1d89fdc430e68f0f3673bfcf7d38
Bill MacAllister <whm@stanford.edu>
2017-05-21 19:42:12 -0700

    Debugging slide table updates

--- cgi-bin/ring_slide_table_action.php
+++ cgi-bin/ring_slide_table_action.php
@@ -9,12 +9,16 @@ require('inc_ring_init.php');
 // Form or URL inputs
 $in_button_update  = get_request('in_button_update');
 $in_up_picture_cnt = get_request('up_picture_cnt', 0);
+$in_start          = get_request('in_start', 0);
+$in_uid            = get_request('in_uid');
 
 // ----------------------------------------------------
 // Main Routine
 
 // ---------------------------------------------------------
 // Processing for updates only
+
+$next_url = 'index.php';
 
 if ( $in_button_update == 'Update' ) {
 
@@ -30,9 +34,10 @@ if ( $in_button_update == 'Update' ) {
         if ($up_od != $up_nd) {
             // Update the meta data
             $sql_cmd = 'UPDATE pictures_information SET ';
-            $sql_cmd .= 'picture_date = ? ';
+            $sql_cmd .= "picture_date = '$up_nd', ";
             $sql_cmd .= 'date_last_maint = NOW() ';
             $sql_cmd .= "WHERE pid = $up_pid ";
+            sys_msg("Updated $up_pid to $up_nd");
             $result = $DBH->query($sql_cmd);
             if (!$result) {
                 sys_err("ERROR:" . $result->error);
@@ -40,15 +45,15 @@ if ( $in_button_update == 'Update' ) {
             }
         }
     }
-
+    $next_url = 'ring_slide_table.php'
+        . '?in_uid=' . $in_uid
+        . '&in_start=' . $in_start;
 } else {
-
-    echo "Ooops, this should never happen!<br>\n";
-
+    sys_err("Invalid request to ring_slide_table_action");
 }
 
 $DBH->close;
 
-header ("REFRESH: 0; URL=ring_slide_table.php");
+header ("REFRESH: 0; URL=$next_url=");
 
 ?>
