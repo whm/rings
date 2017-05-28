9af25570142a442fb97cfb5f20778a2c7e75e222
Bill MacAllister <whm@stanford.edu>
2017-05-27 19:36:33 -0700

    Some css hacking on the maintenance nav menu

--- cgi-bin/page_top.php
+++ cgi-bin/page_top.php
@@ -1,28 +1,28 @@
 <!-- ################# Navigation Sidebar header ################ -->
 
-<div id="maintNav">
+<div class="maintNav">
      <a href="/rings/index.php"><img
        src="/rings-images/icon-home.png"
        alt="Pick a New Ring"
        border="0"></a>
      <br/>
      Pictures
-     <ul>
-     <li><a href="picture_load.php">Load</a></li>
-     <li><a href="picture_reload.php">Re-Load</a></li>
-     <li><a href="picture_maint.php">Maint</a></li>
-     <li><a href="picture_sort.php">Sort</a></li>
-     <li><a href="picture_sort.php?in_new=1">Sort New</a></li>
+     <ul class="maintNavUL">
+     <li class="maintNavLI"><a href="picture_load.php">Load</a></li>
+     <li class="maintNavLI"><a href="picture_reload.php">Re-Load</a></li>
+     <li class="maintNavLI"><a href="picture_maint.php">Maint</a></li>
+     <li class="maintNavLI"><a href="picture_sort.php">Sort</a></li>
+     <li class="maintNavLI"><a href="picture_sort.php?in_new=1">Sort New</a></li>
      </ul>
      People
-     <ul>
-     <li><a href="people_search.php">Find</a></li>
-     <li><a href="people_maint.php">Maint</a></li>
+     <ul class="maintNavUL">
+     <li class="maintNavLI"><a href="people_search.php">Find</a></li>
+     <li class="maintNavLI"><a href="people_maint.php">Maint</a></li>
      </ul>
      Groups
-     <ul>
-     <li><a href="groups.php">List</a></li>
-     <li><a href="group_maint.php">Maint</a></li>
+     <ul class="maintNavUL">
+     <li class="maintNavLI"><a href="groups.php">List</a></li>
+     <li class="maintNavLI"><a href="group_maint.php">Maint</a></li>
      </ul>
 </div>
 <div id="maintContent">
