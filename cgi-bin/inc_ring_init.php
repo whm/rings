9a709dc92c4619fff023a207bbfe345b9e84e908
Bill MacAllister <bill@ca-zephyr.org>
2019-03-18 06:35:17 +0000

    Revert "Update to support and apache load balancer"
    This reverts commit 4d46927ce16a52db22a67fed28730af29ae672ac.
    
    Backout load balancing changes

--- cgi-bin/inc_ring_init.php
+++ cgi-bin/inc_ring_init.php
@@ -9,16 +9,6 @@ function set_default ($fld, $val) {
     return;
 }
 
-function set_session2env($env, $fld) {
-    global $CONF;
-    $val = '';
-    if (!empty($_SERVER[CONF[$env])) {
-        $val = $_SERVER[CONF[$env];
-    }
-    $_SESSION[$fld] = $val;
-    return;
-}
-
 // ---------------------------------------------------------------------
 // Main Routine
 // ---------------------------------------------------------------------
@@ -39,28 +29,23 @@ if (empty($_ENV['RINGCONF'])) {
     $ring_conf = $_ENV['RINGCONF'];
 }
 $CONF = read_conf($ring_conf);
-set_default('cookie_id',        'rings-cookie');
-set_default('db_name',          'rings');
-set_default('db_secret',        '/etc/rings/rings_db.conf');
+set_default('cookie_id',    'rings-cookie');
+set_default('db_name',      'rings');
+set_default('db_secret',    '/etc/rings/rings_db.conf');
 set_default('debug',            0);
 set_default('display_size',     'raw');
-set_default('env_givenname',    'HTTP_X_WEBAUTH_LDAP_GIVENNAME');
-set_default('env_groups',       'HTTP_X_WEBAUTH_LDAP_CZPRIVILEGEGROUP');
-set_default('env_mail',         'HTTP_X_WEBAUTH_LDAP_MAIL');
-set_default('env_remote_user',  'HTTP_X_WEBAUTH_USER');
-set_default('env_sn',           'HTTP_X_WEBAUTH_LDAP_SN');
 set_default('index_size',       '125x125');
 set_default('ldap_server',      'macdir.ca-zephyr.org');
 set_default('mail_domain',      'ca-zephyr.org');
 set_default('mail_size',        '800x600');
 set_default('maint_size',       '640x480');
 set_default('picture_root',     '/srv/rings');
+set_default('ring_admin',       'ring_admin');
 set_default('ring_admin_attr',  'czPrivilegeGroup');
 set_default('ring_admin_group', 'ring:admin');
 set_default('ring_id',          'rings');
 set_default('ring_keytab',      '/NOKEYTAB');
 set_default('ring_princ',       'service/rings');
-set_default('server_admin',     'Bill MacAllister');
 
 // Setup syslog
 openlog('rings-' . $CONF['ring_id'], LOG_PID | LOG_PERROR, LOG_LOCAL3);
@@ -83,21 +68,25 @@ if (empty($_SESSION['msg'])) {
     $_SESSION['msg'] = '';
 }
 
-// set session variables from the environment 
-set_session2env('user_givenname', 'env_givenname');
-set_session2env('user_groups',    'env_groups');
-set_session2env('user_mail',      'env_mail');
-set_session2env('remote_user'     'env_remote_user');
-set_session2env('user_sn',        'env_sn');
-
 // Set the admin flag
-$_SESSION['ring_admin'] = 0;
-my $privs = explode('|', $CONF['env_groups']);
-foreach ($privs as $p) {
-    if ($p == $CONF['ring_admin_group']) {
-        $_SESSION['ring_admin'] = 1;
-        break;
+$ring_admin_group = '';
+if (! empty($_SESSION['ring_admin_group'])
+    && $_SESSION['ring_admin_group'] > 0)
+{
+    $ring_admin_group = $_SESSION['ring_admin_group'];
+} else {
+    $c = 1;
+    while (1==1) {
+        $this_id = 'WEBAUTH_LDAP_CZPRIVILEGEGROUP' . $c;
+        if (empty($_SERVER[$this_id])) {
+            break;
+        }
+        if ($_SERVER[$this_id] == $CONF['ring_admin_group']) {
+            $ring_admin_group = $CONF['ring_admin_group'];
+            break;
+        }
+        $c = $c + 1;
     }
 }
-
+$_SESSION['ring_admin_group'] = $ring_admin_group;
 ?>
